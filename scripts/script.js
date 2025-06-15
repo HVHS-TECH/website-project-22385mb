05c7e82d5b7a903c9cab202085baf7662de17481
22385mb <161382701+22385mb@users.noreply.github.com>
2025-06-15 21:44:18 +0000

    feat: add nav bar and initial dropdown menu

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    const dropdown = document.querySelector(".dropdown");
+    if(dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+    } else {
+        dropdown.classList.add("hidden");
+    }
+})
