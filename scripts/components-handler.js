d3f493ae577e8e6285796ab9159168fdebfe316e
22385mb <161382701+22385mb@users.noreply.github.com>
2025-08-28 02:40:00 +0000

    feat: add custom componnts to other pages and adjust the implementation with the main branch

--- scripts/components-handler.js
+++ scripts/components-handler.js
@@ -19,12 +19,8 @@ navBarTemplate.innerHTML = `
     }
 
     .hamburger {
-        background-color: blue;
-        height: 2rem;
-        width: 2rem;
-
-        justify-self: flex-end;
-        margin: 0.7rem;
+        width: 3rem;
+        margin: 0.1em 0.7em;
     }
 
     .logo img{
@@ -33,11 +29,10 @@ navBarTemplate.innerHTML = `
     }
     </style>
     <nav>
-        <div class="logo">
+        <a class="logo" href="../index.html">
             <img src="../assets/graphics/HVHS-Logo.svg" alt="Hutt Valley High School Logo" />
-        </div>
-        <div class="hamburger">
-        </div>
+        </a>
+        <img class="hamburger" src="../assets/graphics/hamburger-menu.svg" alt="">
     </nav>
 `;
 
@@ -51,39 +46,49 @@ dropDownTemplate.innerHTML = `
     <div class="dropdown hidden">
             <h1 class="title">Music <span class="titleSpan">@ HVHS</span></h1>
             <a class="poutama top right" href="../index.html">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- String-Orchestra -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/string-orchestra_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/string-orchestra_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>Home</p>
             </a>
             <a class="poutama middle centre" href="../html/registration-info.html">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- Guitar-Sheet-Music -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/guitar-sheet-music_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/guitar-sheet-music_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>Registration</p>
             </a>
             <a class="poutama middle right">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- Jazz-Brass-Prizegiving -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/jazz-brass-prizegiving_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/jazz-brass-prizegiving_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>Notices</p>
             </a>
             <a class="poutama bottom left" href="../html/about.html">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- Guitar-Group -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/guitar-group_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/guitar-group_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>About Us</p>
             </a>
             <a class="poutama bottom centre" href="../html/the-staff.html">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- Mrk-conducting -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/mrk-conducting_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/mrk-conducting_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>Our Staff</p>
             </a>
             <a class="poutama bottom right" href="../html/music-groups.html">
-                <img src="../assets/photos/testImg.JPG" alt="an image of musicians playing">
+                <picture> <!-- Volante-Prizegivng -->
+                    <source media="(min-width: 767px)" srcset="../assets/photos/poutama/volante-prizegiving_ls.jpg">
+                    <img class="heroItem poutama" src="../assets/photos/poutama/volante-prizegiving_portrait.jpg" alt="Mr Kovacev conducting the school string orchestra and jazz band at the HVHS 2024 prizegiving">
+                </picture>
                 <p>Musical Groups</p>
             </a>
         </div>
 `;
-
-// Footer Template
-const footerTemplate = document.createElement('template');
-footerTemplate.innerHTML = `
-
-
-`;
-
 
 /***********************************/
 // CLASSES for extending elements
