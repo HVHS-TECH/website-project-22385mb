87abca0b8654dcadc94d124fa5f404a952a859ba
22385mb <161382701+22385mb@users.noreply.github.com>
2025-09-07 04:01:41 +0000

    feat: add staff information cards and functionality for opening and closing them

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,42 @@ document.querySelector("nav-bar").shadowRoot.querySelector(".hamburger").addEven
     }
 })
 
-document.querySelector(".scrollArrow").addEventListener("click", () => {
-    window.scroll({
-        top: 550,
-        left: 0,
-        behavior: "smooth",
+const scrollArrow = document.querySelector("scrollArrow");
+if(scrollArrow != null) {
+    scrollArrow.addEventListener("click", () => {
+        window.scroll({
+            top: 550,
+            left: 0,
+            behavior: "smooth",
+        })
+    })
+}
+
+
+const staffNotes = document.querySelectorAll(".staff-note");
+staffNotes.forEach((note) => {
+    note.addEventListener("click", (event) => {
+        event.stopPropagation();
+        var target = event.target;
+        while(target.classList[0] != "staff-note") {
+            target = target.parentElement;
+        }
+        showStaffInfo(target.attributes["data-staff-member"].value);
     })
 })
+
+document.querySelector(".staff-interactive").addEventListener("click", closeStaffCard , {capture: true});
+
+document.querySelector(".exit-card").addEventListener("click", closeStaffCard);
+
+function showStaffInfo(_staffMember) {
+    var infoCard = document.querySelector(`.info-card#${_staffMember}`);
+    infoCard.classList.add("info-card-show");
+}
+
+function closeStaffCard() {
+    let currentCard = document.querySelector(".info-card-show");
+    if(currentCard != null) {
+        currentCard.classList.remove("info-card-show");
+    }
+}
