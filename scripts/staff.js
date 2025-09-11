12869d1f3133fb5f3ddde1a54454c34590432e51
22385mb <161382701+22385mb@users.noreply.github.com>
2025-09-11 08:49:08 +0000

    feat: add more images and embellishments + fixed up some formatting and comments

--- scripts/staff.js
+++ scripts/staff.js
@@ -1,9 +1,35 @@
 import { animate, scroll } from "https://cdn.jsdelivr.net/npm/motion@12.23.12/+esm";
 
+//Array of distances needed to scroll to reach each section
 var scrollDistances = {}
+
+// On load run scrollingStaff() to set distances also do the same eveyrtime the screen resizes
 window.onload = scrollingStaff();
 window.addEventListener("resize", scrollingStaff)
 
+// For each staffNote add a click event to open the staff info card
+const staffNotes = document.querySelectorAll(".staff-note");
+staffNotes.forEach((note) => {
+    note.addEventListener("click", (event) => {
+        event.stopPropagation();
+        var target = event.target;
+        while(target.classList[0] != "staff-note") {
+            target = target.parentElement;
+        }
+        showStaffInfo(target.attributes["data-staff-member"].value);
+    })
+})
+// Add events for closing the staff info card
+document.querySelector(".staff-interactive").addEventListener("click", closeStaffCard , {capture: true});
+document.querySelector(".exit-card").addEventListener("click", closeStaffCard);
+
+/**********************************/
+// scrollingStaff()
+// Sets up the horizontally scrolling staff feature, figures out distances and lengths of collections
+// Uses Framer Motion Scroll and Animate events to connect the horizontal movement of the element to the user scrolling
+// INPUT
+//      _direction - specify either "left" or "right" to direct which way the slideshow moves 
+/**********************************/
 function scrollingStaff() {
     var staffWidth = 0;
     const staffCollections = document.querySelectorAll(".staff-collection");
@@ -33,40 +59,19 @@ document.querySelectorAll(".scroll-link").forEach((_link) => {
     })
 })
 
+/**********************************/
+// scrollToStaffSection(_section)
+// Scroll to the speicifed section of the staff feature
+// INPUT
+//      _section - the id of the section to go to 
+/**********************************/
 function scrollToStaffSection(_section) {
-    let  sectionDistance = scrollDistances[_section];
-    console.log("scrolled " + window.pageYOffset);
-    console.log(sectionDistance);
-    // document.querySelector(".staff-interactive").scrollIntoView();
-    // setTimeout(() => {
-    //     window.scroll({
-    //         top: window.pageYOffset + scrollDistances[_section],
-    //         left: 0,
-    //         behavior: "smooth",
-    //     })
-    // }, 100);
     window.scroll({
             top: scrollDistances["container"] + scrollDistances[_section],
             left: 0,
             behavior: "smooth",
     })
 }
-
-const staffNotes = document.querySelectorAll(".staff-note");
-staffNotes.forEach((note) => {
-    note.addEventListener("click", (event) => {
-        event.stopPropagation();
-        var target = event.target;
-        while(target.classList[0] != "staff-note") {
-            target = target.parentElement;
-        }
-        showStaffInfo(target.attributes["data-staff-member"].value);
-    })
-})
-
-document.querySelector(".staff-interactive").addEventListener("click", closeStaffCard , {capture: true});
-
-document.querySelector(".exit-card").addEventListener("click", closeStaffCard);
 
 function showStaffInfo(_staffMember) {
     var infoCard = document.querySelector(`.info-card#${_staffMember}`);
