3929714c9b7626a8a7b5dbecae725a62731f77fc
22385mb <161382701+22385mb@users.noreply.github.com>
2025-09-16 10:58:10 +0000

    feat: create initial carousel with animations and interactivity

--- scripts/groups.js
+++ scripts/groups.js
@@ -16,13 +16,14 @@ chevrons.forEach((chevron) => {
 //      _direction - specify either "left" or "right" to direct which way the slideshow moves 
 /**********************************/
 function changeCard(_direction) {
-    //Get the current card and it's number in the order
-    const currentCard = document.querySelector(".card.current");
-    const currentCardNum = Number(currentCard.getAttribute("data-card-num"));
     //Setting up variables for the next card and the card behind it
     var nextCardNum;
     var hiddenCardNum;
     var oppDirection;
+
+    //Get the current card and it's number in the order
+    const currentCard = document.querySelector(".card.current");
+    const currentCardNum = Number(currentCard.getAttribute("data-card-num"));
 
     if(_direction == "left") {
         nextCardNum = checkForSlideEnd(currentCardNum + 1);
@@ -33,22 +34,37 @@ function changeCard(_direction) {
         hiddenCardNum = checkForSlideEnd(nextCardNum - 1);
         oppDirection = "left";
     }
+    var nextCard = document.querySelector(`.card[data-card-num="${nextCardNum}"]`);
+    var hiddenCard = document.querySelector(`.card[data-card-num="${hiddenCardNum}"]`);
+    var lastCard = document.querySelector(`.card.behind.${_direction}`);
 
     // MOVE ALL THE CARDS BY CHANGING THIER CLASSES
     //Move the card on the "end" by making it invisible
-    document.querySelector(`.card.behind.${_direction}`).classList.remove(_direction, "behind");
+    lastCard.classList.remove(_direction, "behind");
     //Make the previous current card move behind in the right direction
     currentCard.classList.remove("current");
     currentCard.classList.add("behind", _direction);
-
     //Get the cards for the next and the hidden card then add the necessary classes to "move them"
-    var nextCard = document.querySelector(`.card[data-card-num="${nextCardNum}"]`);
-    var hiddenCard = document.querySelector(`.card[data-card-num="${hiddenCardNum}"]`);
     nextCard.classList.remove("behind", "left", "right");
     nextCard.classList.add("current");
     hiddenCard.classList.add("behind", oppDirection);
 
+    // ADD THE ANIMATION CLASSES
+    manageAnimClasses(lastCard, `${_direction}-hidden`);
+    manageAnimClasses(currentCard, `center-${_direction}`);
+    manageAnimClasses(nextCard, `${oppDirection}-center`);
+    manageAnimClasses(hiddenCard, `hidden-${oppDirection}`);
+
     changeInfoCard(nextCardNum, currentCardNum);
+}
+
+function manageAnimClasses(_card, _newAnim) {
+    const re = /\w{4,}-\w{4,}/;
+    let classes = Array.from(_card.classList);
+    let animClass = classes.find(value => value.match(re));
+
+    _card.classList.remove(animClass);
+    _card.classList.add(_newAnim);
 }
 
 function checkForSlideEnd(_card) {
