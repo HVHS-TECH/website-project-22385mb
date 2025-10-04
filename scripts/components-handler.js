3bbdf52231b85ae57bb7bb9634c7516f43eb6516
22385mb <161382701+22385mb@users.noreply.github.com>
2025-10-04 09:04:45 +0000

    feat: optimise fonts by linking them instead of importing

--- scripts/components-handler.js
+++ scripts/components-handler.js
@@ -248,3 +248,14 @@ customElements.define('nav-bar', NavBar);
 customElements.define('drop-down-menu', DropDown);
 customElements.define('custom-footer', CustomFooter);
 customElements.define('group-info-card', GroupInfo);
+
+
+// Open and close the nav bar when the "hamburger" symbol is clicked
+document.querySelector("nav-bar").shadowRoot.querySelector(".hamburger").addEventListener("click", () => {
+    const dropdown = document.querySelector("drop-down-menu").shadowRoot.querySelector(".dropdown");
+    if(dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+    } else {
+        dropdown.classList.add("hidden");
+    }
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,3 @@
-// Open and close the nav bar when the "hamburger" symbol is clicked
-document.querySelector("nav-bar").shadowRoot.querySelector(".hamburger").addEventListener("click", () => {
-    const dropdown = document.querySelector("drop-down-menu").shadowRoot.querySelector(".dropdown");
-    if(dropdown.classList.contains("hidden")) {
-        dropdown.classList.remove("hidden");
-    } else {
-        dropdown.classList.add("hidden");
-    }
-})
-
 // When the user clicks the scroll down icon (on home page) the page scrolls
 const scrollArrow = document.querySelector(".scrollArrow");
 if(scrollArrow != null) {
